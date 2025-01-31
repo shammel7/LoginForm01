85afb200c71952e29ff6c77e5daa72197a99560b
shammel7 <shammelquilapio@gmail.com>
2025-01-31 14:49:19 +1200

    Delete script3.js

--- script3.js
+++
@@ -1,9 +0,0 @@
-// script.js
-const logoutButton = document.getElementById('logout-btn');
-
-// Simulate logout action
-logoutButton.addEventListener('click', () => {
-  // Redirect to the login page
-  alert('You are being redirected to the login page.');
-  window.location.href = '/login'; // Change this URL as needed
-});
